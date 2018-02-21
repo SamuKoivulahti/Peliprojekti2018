37d88571d4260004772d1211f5cbf847a91e1188
Essi Supponen <essi.supponen@cs.tamk.fi>
2018-02-21 10:56:34 +0200

    Tämä on testi

--- core/src/fi/tamk/rikollisentunnistus/Rikollisentunnistus.java
+++ core/src/fi/tamk/rikollisentunnistus/Rikollisentunnistus.java
@@ -22,6 +22,7 @@ public class Rikollisentunnistus extends ApplicationAdapter {
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 		batch.draw(img, 0, 0);
+		// testi
 		batch.end();
 	}
 	
