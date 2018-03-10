c4f6f3e5abd0814876762b8dd9d26298e5e02e58
Samu Koivulahti <samu.koivulahti@cs.tamk.fi>
2018-03-10 02:03:16 +0200

    added timer to controls

--- core/src/fi/tamk/rikollisentunnistus/Controls.java
+++ core/src/fi/tamk/rikollisentunnistus/Controls.java
@@ -1,6 +1,7 @@
 package fi.tamk.rikollisentunnistus;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
 
 /**
  * Created by Koivulahti on 5.3.2018.
@@ -29,6 +30,9 @@ public class Controls {
     static boolean isAbleMoveUp = true;
     static boolean isAbleMoveDown = true;
 
+    static boolean timer = false;
+    static Screen currentScreen;
+
     static float accelerometerX() {
         accelX = Gdx.input.getAccelerometerX();
         //Gdx.app.log("TAG", "X:" + accelX);
@@ -41,70 +45,80 @@ public class Controls {
         return accelY;
     }
 
-    static boolean moveRight() {
+    static boolean moveRight(boolean isTimed) {
         accelerometerY();
         if (accelY > moveRight && isAbleMoveRight) {
             isAbleMoveRight = false;
+            timer = isTimed;
             //Gdx.app.log("TAG", "R");
-            return true;
+            return !isTimed;
         } else if (!isAbleMoveRight && accelY < hysteresisRight && accelY > 0) {
             isAbleMoveRight = true;
+            elapsedTime = 0;
             //Gdx.app.log("TAG", "Rback");
+        } else if (accelY > moveRight && !isAbleMoveRight && timer) {
+            return timer();
         }
         return false;
     }
 
-    static boolean moveLeft() {
+    static boolean moveLeft(boolean isTimed) {
         accelerometerY();
         if (accelY < moveLeft && isAbleMoveLeft) {
             isAbleMoveLeft = false;
+            timer = isTimed;
             //Gdx.app.log("TAG", "L");
-            return true;
+            return !isTimed;
         } else if (!isAbleMoveLeft && accelY > hysteresisLeft && accelY < 0) {
             isAbleMoveLeft = true;
+            elapsedTime = 0;
             //Gdx.app.log("TAG", "Lback");
+        }
+        else if (accelY < moveLeft && !isAbleMoveLeft && timer) {
+            return timer();
         }
         return false;
     }
 
-    static boolean moveUp() {
+    static boolean moveUp(boolean isTimed) {
         accelerometerX();
         if (accelX < moveUp && isAbleMoveUp) {
             //Gdx.app.log("TAG", "L");
-            startTimer = true;
+            timer = isTimed;
             isAbleMoveUp = false;
+            return !isTimed;
         } else if (!isAbleMoveUp && accelX > hysteresisUp && accelX < 0) {
             isAbleMoveUp = true;
+            elapsedTime = 0;
             //Gdx.app.log("TAG", "Lback");
-        } else if (accelX < moveUp && !isAbleMoveUp && startTimer) {
-            elapsedTime += Gdx.graphics.getDeltaTime();
-            Gdx.app.log("moveUp", "moveup" + elapsedTime);
-            if (elapsedTime >= 3) {
-                elapsedTime = 0;
-                startTimer = false;
-                return true;
-            }
+        } else if (accelX < moveUp && !isAbleMoveUp && timer) {
+            return timer();
         }
         return false;
     }
 
-    static boolean moveDown() {
+    static boolean moveDown(boolean isTimed) {
         accelerometerX();
         if (accelX > moveDown && isAbleMoveDown) {
-            //Gdx.app.log("TAG", "L");
-            startTimer = true;
+            timer = isTimed;
             isAbleMoveDown = false;
+            return !isTimed;
+
         } else if (!isAbleMoveDown && accelX < hysteresisDown && accelX > 0) {
             isAbleMoveDown = true;
-            //Gdx.app.log("TAG", "Lback");
-        } else if (accelX > moveDown && !isAbleMoveDown && startTimer) {
-            elapsedTime += Gdx.graphics.getDeltaTime();
-            Gdx.app.log("moveDown", "movedown" + elapsedTime);
-            if (elapsedTime >= 3) {
-                elapsedTime = 0;
-                startTimer = false;
-                return true;
-            }
+            elapsedTime = 0;
+        } else if (accelX > moveDown && !isAbleMoveDown && timer) {
+            return timer();
+        }
+        return false;
+    }
+
+    static boolean timer() {
+        elapsedTime += Gdx.graphics.getDeltaTime();
+        if (elapsedTime >= 3) {
+            elapsedTime = 0;
+            timer = false;
+            return true;
         }
         return false;
     }
